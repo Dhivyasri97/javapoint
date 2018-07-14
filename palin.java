c44e54c118691534ba8da2546e88de968e26197e
Dhivyasri97 <36623737+Dhivyasri97@users.noreply.github.com>
2018-07-14 20:17:19 +0530

    Add files via upload

---
+++ palin.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class palin
+{
+public static void main(String args[])
+{
+int n,sum=0,b,r,rn=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+b=n;
+while(n>0)
+{
+r=n%10;
+sum=sum+r;
+rn=rn*10+r;
+n=n/10;
+}
+if(b==rn)
+{
+System.out.println("palindrome");
+}
+else
+{
+System.out.println("not palindrome");
+}
+}
+}
