d90155bd80e4989f2277e3f0084b7c560780c616
Dhivyasri97 <36623737+Dhivyasri97@users.noreply.github.com>
2018-07-14 05:48:22 +0000

    Create v.java

---
+++ v.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class v{ 
+public static void main(String aa[]){
+char n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter char");
+n=s.next().charAt(0); 
+if((n=='a'||n=='e'||n=='i'||n=='o'||n=='u')||(n=='A'||n=='E'||n=='I'||n=='O'||n=='U')){
+System.out.println("vowels");
+}
+else{
+System.out.println("consonants");
+}
