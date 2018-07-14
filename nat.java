e808f9d6adc0e05d4e470b03f5f61f6b046461d6
Dhivyasri97 <36623737+Dhivyasri97@users.noreply.github.com>
2018-07-14 15:35:34 +0530

    Add files via upload

---
+++ nat.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class nat
+{
+public static void main(String args[])
+{
+int a=0,n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter no");
+n=sc.nextInt();
+for(int i=0;i<=n;i++)
+{
+a+=i;
+}
+System.out.println(a);
+}
+}
+
