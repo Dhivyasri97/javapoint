f19501e2d235e9a9b293a32dc98e3210842dba70
Dhivyasri97 <36623737+Dhivyasri97@users.noreply.github.com>
2018-07-15 17:21:39 +0530

    Add files via upload

---
+++ e.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class e
+{
+public static void main(String args[])
+{
+int N,Q;
+Scanner sc=new Scanner(System.in);
+N=sc.nextInt();
+Q=sc.nextInt();
+for(int i=N;i<Q;i++)
+{
+if(i%2==0)
+{
+System.out.println();
+System.out.println(i);
+}
+}
+}
+}
