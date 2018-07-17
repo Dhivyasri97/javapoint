49a9d8357f57e2b73bc05d01b6f038c3445a89a5
Dhivyasri97 <36623737+Dhivyasri97@users.noreply.github.com>
2018-07-17 21:11:40 +0530

    Add files via upload

---
+++ n1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class n1
+{
+public static void main(String args[])
+{
+int d;
+Scanner sc=new Scanner(System.in);
+d=sc.next().charAt(0);
+if(d>='0' &&d <='9')
+{
+System.out.println("Yes");
+}
+else
+{
+System.out.println("No");
+}
+}
+}
